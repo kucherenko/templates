a12dc4ba87a05dd82c9c005a8c492e82701182e1
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2024-08-15 17:57:02 +0300

    feat(react): add react template

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { LandingPage } from "./components/pages/landing/Landing";
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
